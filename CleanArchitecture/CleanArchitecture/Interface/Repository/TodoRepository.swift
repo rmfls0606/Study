dcea0caf31b577afeadd8110ccf44d3f6903fe59
rmfls0606 <rmfls0606@naver.com>
2025-05-14 04:00:00 +0900

    [Interface] UseCase에서 사용할 TodoRepository 프로토콜 정의 (데이터 접근 추상화 목적)

---
+++ CleanArchitecture/CleanArchitecture/Interface/Repository/TodoRepository.swift
@@ -0,0 +1,13 @@
+//
+//  TodoRepository.swift
+//  CleanArchitecture
+//
+//  Created by 이상민 on 5/14/25.
+//
+
+import Foundation
+
+protocol TodoRepository{
+    func fetchTodos() async throws -> [Todo]
+    func addTodo(title: String) async throws
+}
