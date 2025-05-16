701baca0049e1f682b2190f3a9d0ac0b4456f30c
rmfls0606 <rmfls0606@naver.com>
2025-05-17 02:10:43 +0900

    [App] AppDIContainer 생성 및 ViewModel 구성 함수 정의

---
+++ CleanArchitecture/CleanArchitecture/App/AppDIContainer.swift
@@ -0,0 +1,29 @@
+//
+//  AppDIContainer.swift
+//  CleanArchitecture
+//
+//  Created by 이상민 on 5/17/25.
+//
+
+import Foundation
+
+struct AppDIContainer{
+    @MainActor
+    func makeTodoViewModel() -> TodoViewModel{
+        //실제 데이터 소스(현재는 테스트용 메모리 DataSource 사용)
+        let dataSource = TodoRemoteDataSource()
+        
+        //Repository 생성
+        let repository = TodoRepositoryImpl(remoteDataSource: dataSource)
+        
+        //UseCase 생성
+        let fetchUseCase = FetchTodosUseCaseImpl(repository: repository)
+        let addUseCase = AddTodoUseCaseImpl(repository: repository)
+        
+        //ViewModel 생성
+        return TodoViewModel(
+            fetchTodosUseCase: fetchUseCase,
+            addTodoUseCase: addUseCase
+        )
+    }
+}
