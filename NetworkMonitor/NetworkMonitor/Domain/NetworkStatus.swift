544643c88381ba159182bbd44754c4b40d435059
rmfls0606 <rmfls0606@naver.com>
2025-06-03 20:15:28 +0900

    [Domain] 네트워크 상태 및 타입 enum 정의

---
+++ NetworkMonitor/NetworkMonitor/Domain/NetworkStatus.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkStatus.swift
+//  NetworkMonitor
+//
+//  Created by 이상민 on 6/3/25.
+//
+
+import Foundation
+
+enum NetworkStatus{
+    case connected(NetworkType) //어떤 타입으로 연결
+    case disconnected //비연결
+}
+
+enum NetworkType{
+    case wifi //와이파이
+    case cellular //셀룰러
+    case ethernet //이더넷
+    case unknown //알 수 없음
+}
