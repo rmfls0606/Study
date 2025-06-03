f7a308db65dc45c4b9a60f3492a58a406a66d1d4
rmfls0606 <rmfls0606@naver.com>
2025-06-03 14:11:43 +0900

    [Feat] Network 프레임워크를 사용하여 인터넷 연결 상태에 따른 로직처리를 공부하기 위한 프로젝트 생성

---
+++ NetworkMonitor/NetworkMonitor/NetworkMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkMonitorApp.swift
+//  NetworkMonitor
+//
+//  Created by 이상민 on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
